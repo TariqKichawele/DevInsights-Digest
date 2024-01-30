f9bc881a29c9c9e127b44068080c0b1fe98f4f51
TariqKichawele <tariqkichawele66@gmail.com>
2024-01-29 19:31:29 -0500

    add functionality to signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,48 @@
-import { Link } from "react-router-dom"
-import { Button, Label, TextInput } from "flowbite-react"
+import { Link, useNavigate } from "react-router-dom"
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react"
+import { useState } from "react"
 
 export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [ error, setError ] = useState(null);
+  const [ loading, setLoading ] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ 
+      ...formData, 
+      [e.target.id]: e.target.value.trim()
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!formData.username || !formData.email || !formData.password) {
+      return setError('All fields are required');
+    }
+    try {
+      setLoading(true);
+      setError(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData)
+      });
+      const data = await res.json();
+      if(data.success === false) {
+        return setError(data.message);
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -18,7 +59,7 @@ export default function Signup() {
               </span>
               Blog
           </Link>
-          <p className="text-sm mt-5">
+          <p className="text-sm mt-5 leading-6">
             Welcome to Tariq&apos;s - Your Gateway to Insightful Stories and Inspiration
             At Tariq&apos;s, we believe in the power of stories to captivate, inspire, 
             and connect us. Our digital haven is a space where narratives unfold, 
@@ -27,35 +68,43 @@ export default function Signup() {
         </div>
 
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Your username"/>
               <TextInput 
                 type='text'
                 placeholder='Username'
                 id="username"
-
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value="Your email"/>
               <TextInput 
-                type='text'
+                type='email'
                 placeholder='Email'
                 id="email"
-                
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value="Your password"/>
               <TextInput 
-                type='text'
+                type='password'
                 placeholder='Password'
                 id="password"
+                onChange={handleChange}
               />
             </div>
-            <Button gradientDuoTone={'purpleToPink'} type="submit">
-              Sign Up
+            <Button gradientDuoTone={'purpleToPink'} type="submit" disabled={loading}>
+              {
+                loading ? (
+                  <>
+                    <Spinner size={'sm'}/>
+                    <span className="pl-3">Loading...</span>
+                  </>
+                ) : "Sign Up"
+              }
             </Button>
           </form>
 
@@ -65,7 +114,13 @@ export default function Signup() {
                 Sign In
             </Link>
           </div>
-
+          {
+            error && (
+              <Alert className="mt-5" color={'failure'}>
+                {error}
+              </Alert>
+            )
+          }
         </div>
 
       </div>
