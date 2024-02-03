05b0afca46b70c8b53cb163d2189285778589c9a
TariqKichawele <tariqkichawele66@gmail.com>
2024-02-03 14:24:00 -0500

    add posts section to the dashboard

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -71,6 +71,7 @@ export default function CreatePost() {
             setPublishError('Error creating post');
         }
     }
+    
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
         <h1 className="text-center text-3xl my-7 font-semibold">
